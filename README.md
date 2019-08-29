8e12e506e18fb23b8b8c19dbdb6656e4552dac69
teuntjepeeters <44974442+teuntjepeeters@users.noreply.github.com>
2019-08-29 12:12:41 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Startcode_Afvinkopdracht4
+Dit is de startcode nodig voor afvinkopdracht 4 van OWE1a.
---
+++ startOpgave4.py
@@ -0,0 +1,36 @@
+# Iteratie opdracht
+# Start script voor opgave over sikkelcel
+# (c) Martijn van der Bruggen, 2007-2010
+# Updates:
+# November 2010, code bijgewerkt met instructies voor de opdracht
+# Hogeschool van Arnhem en Nijmegen
+
+# Sequenties voor respectievelijk sikkelcel- en normale cellen
+# Bekend is dat het gen coderend voor hemoglobine bij sikkelcel aandoening een andere nucleotide heeft
+# De sequentie voor de sikkelcel en een "gezonde bloedcel" zijn hier onder gegeven
+sikkel_seq = 'GAGCCATCTATTGCTTACATTTGCTTCTGACACAACTGTGTTCACTAGCAACCTCAAACAGACACCATGGTGCACCTGACTCCTGTGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTGGATGAAGTTGGTGGTGAGGCCCTGGGCAGGTTGGTATCAAGGTTACAAGACAGGTTTAAGGAGACCAATAGAAACTGGGCATGTGGAGACAGAGAAGACTCTTGGGTTTCT'
+normaal_seq ='GAGCCATCTATTGCTTACATTTGCTTCTGACACAACTGTGTTCACTAGCAACCTCAAACAGACACCATGGTGCACCTGACTCCTGAGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTGGATGAAGTTGGTGGTGAGGCCCTGGGCAGGTTGGTATCAAGGTTACAAGACAGGTTTAAGGAGACCAATAGAAACTGGGCATGTGGAGACAGAGAAGACTCTTGGGTTTCT'
+
+# In het bestand enzymen. txt staan kandidaat restrictie enzymen
+# Opdracht schrijf een programma dat al deze enzymen doorloopt een suggestie
+# geeft welk restrictie enzym welk knipt in de ene sequentie en niet in de andere sequentie
+# Hiermee kunnen we diagnostisch enzym voorstellen om vast te stellen of een persoon
+# drager is van het sikkelcel allel
+bestand = open ("enzymen.txt")
+
+
+# Aanwijzingen voor het schrijven van je programma
+# -------------------------------------------------------------
+# Het lezen van een regel kan met bestand.readline()
+# Lees door totdat je een lege regel aantreft
+# Een regel bestaat uit twee stukken enzym en knipsequentie. Bijvoorbeeld: DdeI C^TGAG
+# Het opsplitsen van een regel in twee stukken op de spatie kan middels: enzym, seq = regel.split()
+# Door bovenstaande split verkrijg je twee variabelen enzym en seq, respectievelijk de naam van het enzym en de sequentie waar deze in knipt
+# Verwijderen het dakje uit de seq met seq.replace("^","")
+# --------------------------------------------------------------------
+
+# Auteur:
+# Datum:
+# Functie:
+
+
